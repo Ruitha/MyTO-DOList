2bb952937e6ace5effca0a65e0a347faad982a6e
Ruitha <wagemajohnny@gmail.com>
2023-12-11 15:16:13 +0300

    Implementing the Edit button

--- gui.py
+++ gui.py
@@ -4,23 +4,41 @@ import PySimpleGUI as py
 label = py.Text("Type in a to-do")
 input_box = py.InputText(tooltip="Enter to-do", key="todo")
 add_button = py.Button("Add")
+list_box = py.Listbox(values=functions.get_todos(), key='todos',
+                      enable_events=True, size=[45, 10])
+edit_button = py.Button("Edit")
 
 window = py.Window('My TO-DO App',
-                   layout=[[label], [input_box], [add_button]],
+                   layout=[[label], [input_box], [add_button],
+                           [list_box, edit_button]],
                    font=('Helvetica', 10))
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
+    print(1, event)
+    print(2, values)
+    print(3, values['todos'])
     match event:
-        case"Add":
+        case "Add":
             todos = functions.get_todos()
             new_todo = values['todo'] + '\n'
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case 'todos':
+            window['todo'].update(value=values['todos'])
+
         case py.WIN_CLOSED:
             break
 
-
 window.close()
-
